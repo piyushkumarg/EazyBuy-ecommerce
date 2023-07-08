bcc52c624ac2edcba96209de8f628c6826bc3055
Piyush Kumar <83285872+piyushkumarg@users.noreply.github.com>
2023-07-08 19:43:08 +0530

    Backend: Passport Implementation w local / JWT

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { createUser, signinUser } = require("../controller/Auth");
+const { createUser, signinUser, checkUser } = require("../controller/Auth");
+const passport = require("passport");
 
 const router = express.Router();
 //  /auth is already added in base path
-router.post("/signup", createUser).post("/signin", signinUser);
-
+router
+  .post("/signup", createUser)
+  .post("/signin", passport.authenticate("local"), signinUser)
+  .get("/check", passport.authenticate("jwt"), checkUser);
 exports.router = router;
